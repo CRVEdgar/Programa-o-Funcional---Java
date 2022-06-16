a6d53a2c647a53fc46958aa769155fc94809f858
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2022-06-16 16:54:50 -0300

    start project

---
+++ src/main/java/com/example/programacao_funcional/ProgramacaoFuncionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.programacao_funcional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacaoFuncionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramacaoFuncionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/programacao_funcional/ProgramacaoFuncionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programacao_funcional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacaoFuncionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
